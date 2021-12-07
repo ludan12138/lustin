ba7b6cb311db031436eb3595b987661c58ffd83b
ludan <1973575625@qq.com>
2021-12-07 23:59:24 +0800

    add logback

--- lustin-web/src/main/java/com/ludan/LustinApplication.java
+++ lustin-web/src/main/java/com/ludan/LustinApplication.java
@@ -1,5 +1,7 @@
 package com.ludan;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LustinApplication {
 
+    private final Logger logger = LoggerFactory.getLogger(LustinApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(LustinApplication.class,args);
     }
 
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        logger.error("error logback for lustin");
+        logger.info("info logback for lustin");
         return String.format("Hello %s!", name);
     }
 }
